7560e880afa69f9d05ec9ddd869a9d7f881a2cbf
RegisLewis <regisaslewis@gmail.com>
2023-02-12 00:40:32 -0800

    I hope this is right

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const userConfigObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch("http://localhost:3000/users", userConfigObject)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (object) {
+                document.body.textContent = object.id;
+            })
+            .catch(function (error) {
+                document.body.textContent = error.message;
+            });
+}
